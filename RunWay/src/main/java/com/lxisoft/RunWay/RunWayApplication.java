726a407b815ce60c1b5f440dd4b5d8a0e8c9c74e
Pavana <pavana.n.p@lxisoft.com>
2019-12-03 16:24:15 +0530

    IN-2886

---
+++ RunWay/src/main/java/com/lxisoft/RunWay/RunWayApplication.java
@@ -0,0 +1,13 @@
+package com.lxisoft.RunWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunWayApplication.class, args);
+	}
+
+}
